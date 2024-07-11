b6e02d9112169707b325408912187268d0aa0a6d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-11 11:52:25 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/index.dart
+++ lib/custom_code/actions/index.dart
@@ -3,3 +3,5 @@ export 'upload_local_file_to_firebase_storage.dart'
 export 'upload_data_with_progress.dart' show uploadDataWithProgress;
 export 'select_files_with_allowed_extensions.dart'
     show selectFilesWithAllowedExtensions;
+export 'upload_selected_file_with_progress.dart'
+    show uploadSelectedFileWithProgress;
--- lib/select_and_upload_with_progress/select_and_upload_with_progress_model.dart
+++ lib/select_and_upload_with_progress/select_and_upload_with_progress_model.dart
@@ -1,3 +1,4 @@
+import '/backend/backend.dart';
 import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'select_and_upload_with_progress_widget.dart'
@@ -6,11 +7,30 @@ import 'package:flutter/material.dart';
 
 class SelectAndUploadWithProgressModel
     extends FlutterFlowModel<SelectAndUploadWithProgressWidget> {
+  ///  Local state fields for this page.
+
+  int ii = 0;
+
+  List<UploadingFileStruct> uploadingFiles = [];
+  void addToUploadingFiles(UploadingFileStruct item) =>
+      uploadingFiles.add(item);
+  void removeFromUploadingFiles(UploadingFileStruct item) =>
+      uploadingFiles.remove(item);
+  void removeAtIndexFromUploadingFiles(int index) =>
+      uploadingFiles.removeAt(index);
+  void insertAtIndexInUploadingFiles(int index, UploadingFileStruct item) =>
+      uploadingFiles.insert(index, item);
+  void updateUploadingFilesAtIndex(
+          int index, Function(UploadingFileStruct) updateFn) =>
+      uploadingFiles[index] = updateFn(uploadingFiles[index]);
+
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
-  // Stores action output result for [Custom Action - selectFilesWithAllowedExtensions] action in Button widget.
+  // Stores action output result for [Custom Action - selectFilesWithAllowedExtensions] action in SelectFilesNUploadWithProgress widget.
   List<SelectedFileStruct>? selectedFiles;
+  // Stores action output result for [Custom Action - uploadSelectedFileWithProgress] action in SelectFilesNUploadWithProgress widget.
+  UploadedFileStruct? uploadedFile;
 
   @override
   void initState(BuildContext context) {}
