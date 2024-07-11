8c01fb9074cc2b1f0157c3a38be386c7eda04b6a
Matija Stepanic <stepanic.matija@gmail.com>
2024-07-11 14:40:15 +0200

    Local changes before sync

--- lib/custom_code/actions/upload_selected_file_with_progress.dart
+++ lib/custom_code/actions/upload_selected_file_with_progress.dart
@@ -11,9 +11,40 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:mime_type/mime_type.dart';
+
 Future<UploadedFileStruct?> uploadSelectedFileWithProgress(
   SelectedFileStruct selectedFile,
   Future Function(double? progress)? onProgress,
 ) async {
   // Add your function code here!
+
+  String path = selectedFile.storagePath;
+  Uint8List data = Uint8List.fromList(selectedFile.bytes);
+
+  if (onProgress != null) {
+    onProgress(0);
+  }
+
+  final storageRef = FirebaseStorage.instance.ref().child(path);
+  final metadata = SettableMetadata(contentType: mime(path));
+  final uploadTask = storageRef.putData(data, metadata);
+
+  uploadTask.snapshotEvents.listen((event) {
+    if (onProgress != null) {
+      onProgress(event.bytesTransferred / event.totalBytes);
+    }
+  });
+
+  final result = await uploadTask;
+
+  result.
+
+  final uploadedFile = UploadedFileStruct(
+    localName: selectedFile.filePath,
+    storageName: selectedFile.storagePath,
+  );
+
+  return result.state == TaskState.success ? result.ref.getDownloadURL() : null;
 }
